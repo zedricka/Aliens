011d88725a9c3030a7607dca904732774c21df78
zedricka <zedrick.ara@gmail.com>
2016-10-24 08:09:40 -1000

    AlienNoises

---
+++ index.js
@@ -0,0 +1,46 @@
+First we put them into variables
+
+Total amountal of aliens are 10,000
+
+Use var aliens = "10,000"
+
+if there are more than 5,000 green aliens
+write console.log "No,why?????" 
+
+if there are more than a total of 8,000 green and
+red aliens combined
+write console.log w/ "Ugh, why are they so ugly?"
+
+not more than 3000 red and blue aliens combined
+write console.log "Okay, this seems somewhat manageable. 
+Wait no, we're still dead..."
+
+You may assign alien amounts yourself (Must add up to 10,000)
+
+Use 
+var name = "??"
+if (name == ??) {
+  console.log ("CommentHere")
+}else if(){
+  console.log()
+}
+
+==================================================
+var green = 7000;
+var red = 1000;
+var blue = 2000;
+
+var aliens = green + red + blue;
+
+if (aliens == 10000){
+  console.log("Wow so many aliens");
+}
+if (green > 5000){
+  console.log ("No, why!");
+}
+if (green + red >= 8000){
+  console.log("Ugh, why are they so ugly?");
+}
+if (red && blue < 3000) {
+  console.log ("Okay, this seems somewhat manageable. Wait no, we're still dead...");
+}
